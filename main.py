3217e36bc09c4a94147ea5f796037bbeec4ccf17
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-09-23 17:05:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import label_process
 path = input('Please inpute path：') 
 frame_rate = input('Please inpute frame rate(frame rate should be input by decimals):')
 
-：')
-
-# you can also get user's own homeline by using line 9 as  
+# you can also get user's own homeline by using line 9 as:  
 # get.twitter_images(path)
 get.search_images(path)
 get.image_process(path, 900, 600)
