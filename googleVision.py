2757897573c938ecf2dc75663f5da912a3e75ad1
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-09-22 22:45:17 -0400

    Add files via upload

--- googleVision.py
+++ googleVision.py
@@ -3,9 +3,14 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 from PIL import Image
+import subprocess
 
 # use Google Vision to get labels from each image.
 def label(file):
+    path = os.getcwd()
+    # edit and add your own json file here.
+    cmd = "set GOOGLE_APPLICATION_CREDENTIALS=" + path + "\My First Project-bbdc176f5fe1.json"
+    subprocess.call(cmd, shell=True)
     client = vision.ImageAnnotatorClient()
     with io.open(file, 'rb') as image_file:
         content = image_file.read()
