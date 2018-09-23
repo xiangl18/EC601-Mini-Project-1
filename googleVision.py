cd055b0763927157f2005fd720796301587b785f
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-09-23 12:59:34 -0400

    Add files via upload
    fix the issue "fail to add json and set the PATH ".
    add warning function in case the json is invalid.

--- googleVision.py
+++ googleVision.py
@@ -2,16 +2,22 @@ import io
 import os
 from google.cloud import vision
 from google.cloud.vision import types
+from google.oauth2 import service_account
 from PIL import Image
-import subprocess
+
 
 # use Google Vision to get labels from each image.
 def label(file):
     path = os.getcwd()
-    # edit and add your own json file here.
-    cmd = "set GOOGLE_APPLICATION_CREDENTIALS=" + path + "\My First Project-bbdc176f5fe1.json"
-    subprocess.call(cmd, shell=True)
-    client = vision.ImageAnnotatorClient()
+    # Please add you json into the folder with all other code files.
+    # edit and add your own json file here,replace the "\My First Project-bbdc176f5fe1.json".
+    try:
+        filename = path + "\My First Project-bbdc176f5fe1.json"
+        credentials = service_account.Credentials.from_service_account_file(filename)
+        client = vision.ImageAnnotatorClient(credentials=credentials)
+    except:
+        raise Warning("fail to add the json file")
+
     with io.open(file, 'rb') as image_file:
         content = image_file.read()
 
