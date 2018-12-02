9b0fac9a874a7d3f0e989f8ddec3eaef78a81e52
xiangl18 <43141808+xiangl18@users.noreply.github.com>
2018-12-01 21:24:11 -0500

    add get_key function test

--- mini_project3_api/test1.py
+++ mini_project3_api/test1.py
@@ -1,9 +1,16 @@
 import api
 
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+json_file = ''
 
 api = api.DatabaseAPI()
+api.get_keys(consumer_key, consumer_secret, access_token, access_token_secret, json_file)
 api.twitter_images(200)
 api.label()
 api.process_video()
 api.search('car')
 api.statistics()
+
